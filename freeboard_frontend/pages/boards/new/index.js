b2a31746cbbaf08875a9ee52d5f6298c5b7d14dc
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2021-11-04 19:32:04 +0900

    1104 게시물등록 포토폴리오 graphql

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -25,95 +25,114 @@
   UploadButton
 } from "../../../../freeboard_frontend/styles/emotion";
 
-
+  import {useMutation,gql} from '@apollo/client'
   import { useState } from 'react'
 
+  const CREATE_BOARD =gql`
+    mutation createBoard($createBoardInput: CreateBoardInput! ){
+      createBoard(createBoardInput:$createBoardInput){
+        _id
+        writer
+        title
+        contents
+      }
+    }
+  `
   export default function BoardsNewPage() {
 
-    const[writer, setWriter] = useState("")
-    const[writerError, setwriterError]= useState("")
-    const[password, setPassword] = useState("")
+    const[createBoard] =useMutation(CREATE_BOARD)
+
+    const[myWriter, setMyWriter]=useState("")
+    const[myPassword, setMyPassword]=useState("")
+    const[myTitle, setMyTitle]=useState("")
+    const[myContents, setMyContents]=useState("")
+    const[writerError, setWriterError]= useState("")
     const[passwordError, setPasswordError]= useState("")
-    const[subject, setSubject] = useState("")
     const[subjectError, setSubjectError]= useState("")
-    const[contents, setContents] = useState("")
     const[contentsError, setContentsError]= useState("")
-    const[youtube, setYoutube] = useState("")
-    const[youtubeError, setYoutubeError]= useState("")
     
-    function aaa(event){
-      setWriter(event.target.value)
-      
-    }
+    
+    function onChangeMyWriter(event){
+        
+      setMyWriter(event.target.value)
 
-    function ppp(event){
-      setPassword(event.target.value)
-      
-    }
+  }
+    function onChangeMyPassword(event){
+        
+      setMyPassword(event.target.value)
 
-    function sss(event){
-      setSubject(event.target.value)
-      
-    }
+  }
 
-    function ttt(event){
-      setContents(event.target.value)
-      
-    }
-    function uuu(event){
-      setYoutube(event.target.value)
-      
-    }
+    function onChangeMyTitle(event){
+          
+      setMyTitle(event.target.value)
 
-    function ccc(){
-      
-      if(writer.length < 1){
-        setwriterError("한글자 이상 입력해주세요")
-      }
-      
-      if(password.length < 4){
+  }
+    function onChangeMyContents(event){
+        
+      setMyContents(event.target.value)
+
+  }
+
+
+      async function submit(){
+
+        if(myWriter.length < 1){
+          setWriterError("한글자 이상 입력해주세요")
+        }
+        
+        if(myPassword.length < 4){
         setPasswordError("네글자 이상 입력해주세요")
+        }
+
+        if(myTitle.length < 5){
+          setSubjectError("다섯글자 이상 입력해주세요")
+        }
+
+        if(myContents.length < 5){
+          setContentsError("다섯글자 이상 입력해주세요")
+        }
+
+
+        const result = await createBoard({
+          variables:{
+            createBoardInput: {
+              writer: myWriter,
+              password : myPassword,
+              title: myTitle,
+              contents: myContents
+            }
+          }
+        })
+        console.log(result)
       }
-
-      if(subject.length < 5){
-        setSubjectError("다섯글자 이상 입력해주세요")
-      }
-
-      if(contents.length < 5){
-        setContentsError("다섯글자 이상 입력해주세요")
-      }
-
-      if(youtube.length < 5){
-        setYoutubeError("URL을 입력해주세요")
-      }
-
-
-   }
+        
     
     return (
+
       <Wrapper>
         <Title>게시판 등록</Title>
         <WriterWrapper>
           <InputWrapper>
             <Label>작성자</Label>
-            <Writer type="text" placeholder="이름을 적어주세요." onChange={aaa}/>
-            <Err>{writerError}</Err>
+            <Writer type="text" placeholder="이름을 적어주세요." onChange={onChangeMyWriter}/>
+             <Err>{writerError}</Err> 
           </InputWrapper>
           <InputWrapper>
             <Label>비밀번호</Label>
-            <Password type="password" placeholder="비밀번호를 적어주세요."   onChange={ppp}/>
-            <Err>{passwordError}</Err>
+            <Password type="password" placeholder="비밀번호를 적어주세요."   onChange={onChangeMyPassword}/>
+            <Err>{passwordError}</Err> 
           </InputWrapper>
         </WriterWrapper>
         <InputWrapper>
           <Label>제목</Label>
-          <Subject type="text" placeholder="제목을 작성해주세요." onChange={sss}/>
-          <Err>{subjectError}</Err>
+          <Subject type="text" placeholder="제목을 작성해주세요." onChange={onChangeMyTitle}/>
+           <Err>{subjectError}</Err> 
         </InputWrapper>
         <InputWrapper>
           <Label>내용</Label>
-          <Contents placeholder="내용을 작성해주세요."  onChange={ttt}/>
-          <Err>{contentsError}</Err>
+          <Contents placeholder="내용을 작성해주세요."  onChange={onChangeMyContents}/>
+          <Err>{contentsError}</Err> 
         </InputWrapper>
         <InputWrapper>
           <Label>주소</Label>
@@ -126,8 +145,7 @@
         </InputWrapper>
         <InputWrapper>
           <Label>유튜브</Label>
-          <Youtube placeholder="링크를 복사해주세요." onChange={uuu}/>
-          <Err>{youtubeError}</Err>
+          <Youtube placeholder="링크를 복사해주세요."/>
         </InputWrapper>
         <ImageWrapper>
           <Label>사진첨부</Label>
@@ -152,11 +170,12 @@
           <RadioLabel htmlFor="image">사진</RadioLabel>
         </OptionWrapper>
         <ButtonWrapper>
-          <SubmitButton onClick={ccc}>등록하기</SubmitButton>
+          <SubmitButton onClick={submit}>등록하기</SubmitButton>
         </ButtonWrapper>
       </Wrapper>
-    );
+    )
     
    }
+   
   
   
