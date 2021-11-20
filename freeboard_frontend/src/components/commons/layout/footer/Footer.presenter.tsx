13b5f0fec0530283ad6e5638c1d89a6d927e8261
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2021-11-20 19:50:52 +0900

    footer 수정

--- freeboard_frontend/src/components/commons/layout/footer/Footer.presenter.tsx
+++ freeboard_frontend/src/components/commons/layout/footer/Footer.presenter.tsx
@@ -1,5 +1,16 @@
 import { Footer } from "./Footer.styles";
 
 export default function FooterUI() {
-  return <Footer></Footer>;
+  return (
+    <>
+      <Footer>
+        <span>MING MARKET</span>
+        <br />
+        <span>
+          밍마켓은 통신판매중개자로서 통신판매 당사자가 아니며, 판매자가 등록한
+          상품정보 및 거래에 브랜디는 책임을 지지 않습니다.
+        </span>
+      </Footer>
+    </>
+  );
 }
