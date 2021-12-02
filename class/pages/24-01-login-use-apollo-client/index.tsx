9c82089b2ccfdc03e515ba87ee15637c8c86c09c
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2021-12-02 12:56:32 +0900

    useApolloClient 로그인 적용

--- class/pages/24-01-login-use-apollo-client/index.tsx
+++ class/pages/24-01-login-use-apollo-client/index.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useContext, useState } from "react";
-import { gql, useMutation } from "@apollo/client";
+import { gql, useApolloClient, useMutation } from "@apollo/client";
 import {
   IMutation,
   IMutationLoginUserArgs,
@@ -14,8 +14,19 @@ const LOGIN_USER = gql`
     }
   }
 `;
+
+const FETCH_USER_LOGGED_IN = gql`
+  query fetchUserLoggedIn {
+    fetchUserLoggedIn {
+      email
+      name
+      picture
+    }
+  }
+`;
+
 export default function LoginPage() {
-  const { setMyAccesToken } = useContext(GlobalContext);
+  const { setMyAccesToken, setMyUserInfo } = useContext(GlobalContext);
   const router = useRouter();
   const [myEamil, setMyEmail] = useState("");
   const [myPassword, setMyPassword] = useState("");
@@ -23,6 +34,9 @@ export default function LoginPage() {
     Pick<IMutation, "loginUser">,
     IMutationLoginUserArgs
   >(LOGIN_USER); //onClickLogin 부분
+
+  //12/02 1
+  const client = useApolloClient();
 
   function onChangeMyEmail(event: ChangeEvent<HTMLInputElement>) {
     setMyEmail(event.target.value);
@@ -39,20 +53,26 @@ export default function LoginPage() {
         password: myPassword,
       },
     });
-    localStorage.setItem(
-      "accessToken",
-      result.data?.loginUser.accessToken || ""
-    );
-    setMyAccesToken?.(result.data?.loginUser.accessToken || "");
+    const accessToken = result.data?.loginUser.accessToken || "";
+    localStorage.setItem("accessToken", accessToken);
+    setMyAccesToken?.(accessToken);
     // 여기서 setAccesToken 필요! (글로벌 스테이트에..)
 
     //const result=await axios.get("koreanjson.com/post/1") 이러한 방식으로 원하는 곳에서
     //useQuery 필요!!
-    //const result = fetchUserLoggedIn()
-    //setUserInfo(result.data?.fetchUserLoggedIn)
+    //12/02 2  (위에 result가 있어서 resultUserInfo 입력해줌)
+    const resultUserInfo = await client.query({
+      query: FETCH_USER_LOGGED_IN,
+      context: {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      },
+    });
+    setMyUserInfo(resultUserInfo.data.fetchUserLoggedIn);
 
     //로그인 성공된 페이지로 이동시키기
-    router.push("/23-05-login-success");
+    router.push("/24-02-login-success");
   }
   return (
     // <LoginForm>
