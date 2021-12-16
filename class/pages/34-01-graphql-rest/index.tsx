c784a7f0e2b3d60404cef4e3d46eb569e9e19c99
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2021-12-16 12:45:02 +0900

    graphqlrestpage

---
+++ class/pages/34-01-graphql-rest/index.tsx
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export default function GraphqlRestPage() {
+  async function onClickSubmit() {
+    const result = await axios.post(
+      "https://backend04.codebootcamp.co.kr/graphql",
+      {
+        query: `
+        mutation createBoard{
+            createBoard(createBoardInput:{
+                writer: "밍",
+                password:"1234",
+                 title:"ww",
+                 contents:"내용"
+                }
+                ){
+                _id
+                 writer
+                }
+            }
+    `,
+      }
+    );
+    console.log(result);
+  }
+
+  return (
+    <>
+      <button onClick={onClickSubmit}>등록하기</button>
+    </>
+  );
+}
