f3af5e2150d5f4b594e8bf40b17f5d659f9ffc08
chanmi94 <83502510+chanmi94@users.noreply.github.com>
2022-01-29 23:43:03 +0900

    class 폴더 수정작업

--- class/src/components/units/21-context-api/MyBoardWrite.container.tsx
+++ class/src/components/units/21-context-api/MyBoardWrite.container.tsx
@@ -1,6 +1,5 @@
 import MyBoardWriteUI from "./MyBoardWrite.presenter";
 
 export default function MyBoardWrite() {
-  // return <MyBoardWriteUI isEdit={props.isEdit} />;
   return <MyBoardWriteUI />;
 }
